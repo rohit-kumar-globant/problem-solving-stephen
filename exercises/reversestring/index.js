6e64b6cab554df157db39135cf46efb4a1ca056a
rohit-kumar-globant <108795419+rohit-kumar-globant@users.noreply.github.com>
2023-06-08 15:54:39 +0530

    first changes

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,13 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    let reversed = '';
+
+    for (let character of str) {
+        reversed = character + reversed
+    }
+    return reversed
+}
 
 module.exports = reverse;
